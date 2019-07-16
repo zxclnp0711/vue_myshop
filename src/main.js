90a127c2af73ab64152262fa35900d2f60786d17
lichenfeng <2277815065@qq.com>
2019-07-16 16:38:01 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import {
+  Button,
+  Row
+} from 'element-ui'
 
+Vue.use(Button)
+Vue.use(Row)
 Vue.config.productionTip = false
 
 new Vue({
